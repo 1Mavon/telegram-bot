0144dec233b0351b9b047f7a591ecc0356f0bce5
1Mavon <3busy3@gmail.com>
2026-05-19 14:14:08 +0500

    fixed render deploy

--- bot.py
+++ bot.py
@@ -1,8 +1,18 @@
 import telebot
+from flask import Flask
+from threading import Thread
+import os
 
 TOKEN = '8855631374:AAGmsjmQdgOqKUFeYhztd9N5xBqLrh3aQuA'
 
 bot = telebot.TeleBot(TOKEN)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Bot is running!"
 
 
 @bot.message_handler(content_types=['text'])
@@ -10,7 +20,7 @@ def handle_text(message):
 
     if message.text.lower() == "хочу лист":
 
-        file = open("ШОПИНГ-ЛИСТ by polifees🐈‍⬛.pdf", "rb")
+        file = open("outfits.pdf", "rb")
 
         bot.send_document(
             message.chat.id,
@@ -25,4 +35,13 @@ def handle_text(message):
         )
 
 
-bot.polling(none_stop=True)
+def run_bot():
+    bot.infinity_polling()
+
+
+bot_thread = Thread(target=run_bot)
+bot_thread.start()
+
+
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
